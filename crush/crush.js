bc598839b80b310147b29bf8e51a576997553be0
Teddy Cross <tkazec@gmail.com>
2012-05-13 21:21:47 -0400

    Flesh out step #3

--- crush/crush.js
+++ crush/crush.js
@@ -64,8 +64,22 @@ var crush = function (code) {
 	// Get the most popular type of quote to minimize escaping in the output string
 	var quote = code.split("'").length < code.split('"').length ? "'" : '"';
 	
+	// Create the output template, using actual code
+	var out = function () {
+		f="{code}";
+		for(i in g="{Z}")
+			e=f.split(g[i]),f=e.join(e.pop());
+		eval(f)
+	};
 	
-	/*** 4 ***/
-	// Create the output
-	return "f=" + quote + code.replace(/[\r\n\\]/g, "\\$&").replace(RegExp(quote, "g"), "\\" + quote) + quote + ";for(i in g=" + quote + Z + quote + ")e=f.split(g[i]),f=e.join(e.pop());eval(f)";
+	// Properly escape the crushed code
+	code = code.replace(/[\r\n\\]/g, "\\$&").replace(RegExp(quote, "g"), "\\" + quote);
+	
+	// Convert the output template to a string, replace bits accordingly, and return the final output
+	return out.toString()
+		.replace(/.+\{((?:.|\s)+)\}/, "$1") // Remove the function wrapper
+		.replace(/[\t\r\n]/g, "") // Remove tabs and newlines
+		.replace(/"/g, quote) // Use correct quotes
+		.split("{Z}").join(Z) // Insert <something>
+		.split("{code}").join(code); // Use split/join instead of replace to avoid replacement patterns!
 };
