c83141802199683dc464058dea1f466ebb37e944
Teddy Cross <tkazec@gmail.com>
2012-08-20 19:25:13 -0400

    Figure out the last of the crush algorithm

--- javascript-crushers-exposed/crush.js
+++ javascript-crushers-exposed/crush.js
@@ -1,6 +1,6 @@
 var crush = function (code) {
 	/*** Step 1: Analyze ***/
-	var M = code.length / 2;
+	var sub_len_max = code.length / 2;
 	var free = [];
 	
 	// Get all safe ASCII characters that aren't already used in the code.
@@ -13,25 +13,41 @@ var crush = function (code) {
 	}
 	
 	// Searches the code for the best possible replacement.
-	var search = function (i) {
-		var best = false;
+	var search = function () {
 		var strs = {};
-		var best_savings = 0;
-		var best_count = 0;
 		
-		// get every possible substring?
-		for (var y = 2, z = M; y <= z; ++y) {
-			for (h = 0, l = i.length - y; h < l; ++h) {
-				if (!strs[e = i.substr(h, y)]) {
-					for (strs[e] = 1, f = h; ~(f = i.indexOf(e, f + y)); ) {
-						strs[e]++;
-						M = y;
+		// Get every possible substring. The main loop increments the size of the substrings to find.
+		// It starts at 2, because we can't use any smaller than that. It stops at sub_len_max, because we
+		// can't use anything larger than that.
+		for (var sub_len = 2, sub_len_stop = sub_len_max; sub_len <= sub_len_stop; ++sub_len) {
+			// This loop gets and counts every substring of length sub_len in the code.
+			// q: why < and not <=?
+			for (var code_pos = 0, code_pos_stop = code.length - sub_len; code_pos < code_pos_stop; ++code_pos) {
+				var sub = code.substr(code_pos, sub_len);
+				var last_pos = code_pos;
+				
+				// Make sure this is the first time we've found the substring.
+				if (!strs[sub]) {
+					strs[sub] = 1;
+					
+					// Count the instances in the code, using a "hopping" method.
+					// Starts from code_pos, then hops along using last_pos.
+					// Ends when it is no longer found.
+					// Also sets the max substring length once it's done. Since code_pos goes up from 0,
+					// this will end up being the longest substring. So don't check for longer later.
+					while (~(last_pos = code.indexOf(sub, last_pos + sub_len))) {
+						strs[sub]++;
+						sub_len_max = sub_len;
 					}
 				}
 			}
 		}
 		
-		// Sort the substrings.
+		// Find the best substring.
+		var best = false;
+		var best_savings = 0;
+		var best_count = 0;
+		
 		for (var sub in strs) {
 			var count = strs[sub];
 			
@@ -42,6 +58,7 @@ var crush = function (code) {
 				var savings = (bytes * (count - 1)) - count - 2;
 				
 				// If the savings are better than any previous substring, update the best.
+				// q: why is a lower count better?
 				if (savings > best_savings || (savings === best_savings && count < best_count)) {
 					best_savings = savings;
 					best_count = count;
@@ -50,7 +67,6 @@ var crush = function (code) {
 			}
 		}
 		
-		// Return the best substring we can replace.
 		return best;
 	};
 	
@@ -61,7 +77,7 @@ var crush = function (code) {
 	var substr;
 	
 	// While we still have free characters and worthwhile replacements...
-	while ((chr = free.pop()) && (substr = search(code))) {
+	while ((chr = free.pop()) && (substr = search())) {
 		// Replace substrings with single characters, and leave behind a copy of the original.
 		code = code.split(substr).join(chr) + chr + substr;
 		used = chr + used;
